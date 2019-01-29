c4af725d7afcd802fcd66c339265c7c1dd23779f
John Fitz <johnfitz79@gmail.com>
2019-01-29 13:08:47 -0500

    Uploaded js practice for HW

---
+++ practice.js
@@ -0,0 +1,24 @@
+//print your name
+//store your name in a variable
+//print the variable
+//print the values 6+6-3
+//print your name 10 times
+//store two numbers in two variables
+//add those numbers together and store in another variable
+//print the value of that last variable
+//write some text and comment it out
+//write something that produces an error
+//define a const to be 'Wyncode'
+console.log("John");
+let name = "John";
+console.log(name);
+console.log(6+6-3);
+let a = 3, b = 5;
+let c = a + b;
+console.log(c);
+//Some text
+const SCHOOL = 'Wyncode'
+for (let counter = 1; counter <= 10; counter++)
+{
+    console.log('John');
+}
